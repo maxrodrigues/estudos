743d3d60298750c51071688a207b9edf631a5cf2
Max Rodrigues <maxuel.rodrigues@atlastechnol.com>
2024-10-10 15:24:46 -0300

    create user model

---
+++ mf-user_management/src/app/Models/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Maxrodrigues\Usermanagement\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
