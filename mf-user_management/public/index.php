1aabfffd371331547a26808838f6c812f5f7a6ac
Max Rodrigues <maxuel.rodrigues@atlastechnol.com>
2024-10-10 15:14:22 -0300

    create new routes and configure eloquent

--- mf-user_management/public/index.php
+++ mf-user_management/public/index.php
@@ -1,19 +1,23 @@
-<?php
+<?php declare(strict_types=1);
 
-use Maxrodrigues\Usermanagement\App\Http\Controller\UsersController;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+
+use Maxrodrigues\Usermanagement\Controller\UsersController;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+require '../config/bootstrap.php';
+
 $app = AppFactory::create();
 
-//$App->get('/', function (Request $request, Response $response, $args) {
+//$app->get('/', function (Request $request, Response $response, $args) {
 //    $response->getBody()->write("Hello world!");
 //    return $response;
 //});
 
 $app->get('/', [UsersController::class, 'index']);
+$app->post('/', [UsersController::class, 'store']);
+$app->put('/{id}', [UsersController::class, 'update']);
+$app->delete('/{id}', [UsersController::class, 'destroy']);
 
 $app->run();
