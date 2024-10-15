64c103548db0affdf1914fa2075ca17decf410db
Max Rodrigues <maxuel.rodrigues@atlastechnol.com>
2024-10-15 17:25:51 -0300

    feat: install api

--- la-user-management/routes/api.php
+++ la-user-management/routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\Notifications\CreateNotificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/', \App\Http\Controllers\Api\Users\ListController::class);
+Route::get('/{user}', \App\Http\Controllers\Api\Users\GetUserController::class);
+Route::post('/', \App\Http\Controllers\Api\Users\StoreController::class);
+Route::put('/{user}', \App\Http\Controllers\Api\Users\UpdateController::class);
+Route::patch('/{user}/status', \App\Http\Controllers\Api\Users\UpdateStatusController::class);
+Route::delete('/{user}', \App\Http\Controllers\Api\Users\DeleteController::class);
+
+
+Route::post('notification', CreateNotificationController::class);
+Route::get('/user/{user}/notification', \App\Http\Controllers\Api\Notifications\GetUserNotificationController::class);
