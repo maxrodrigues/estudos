5fc299e54454335ded0dd734f91a17675ce25223
Max Rodrigues <maxuel.rodrigues@atlastechnol.com>
2024-10-15 17:58:32 -0300

    feat: group routes

--- la-user-management/routes/api.php
+++ la-user-management/routes/api.php
@@ -4,12 +4,17 @@ use App\Http\Controllers\Api\Notifications\CreateNotificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', \App\Http\Controllers\Api\Users\ListController::class);
-Route::get('/{user}', \App\Http\Controllers\Api\Users\GetUserController::class);
-Route::post('/', \App\Http\Controllers\Api\Users\StoreController::class);
-Route::put('/{user}', \App\Http\Controllers\Api\Users\UpdateController::class);
-Route::patch('/{user}/status', \App\Http\Controllers\Api\Users\UpdateStatusController::class);
-Route::delete('/{user}', \App\Http\Controllers\Api\Users\DeleteController::class);
+
+Route::prefix('user')->group(function () {
+    Route::post('/', \App\Http\Controllers\Api\Users\StoreController::class)->name('user.store');
+    Route::get('/', \App\Http\Controllers\Api\Users\ListController::class);
+    Route::get('/{user}', \App\Http\Controllers\Api\Users\GetUserController::class);
+    Route::put('/{user}', \App\Http\Controllers\Api\Users\UpdateController::class);
+    Route::patch('/{user}/status', \App\Http\Controllers\Api\Users\UpdateStatusController::class);
+    Route::delete('/{user}', \App\Http\Controllers\Api\Users\DeleteController::class);
+});
+
+
 
 
 Route::post('notification', CreateNotificationController::class);
